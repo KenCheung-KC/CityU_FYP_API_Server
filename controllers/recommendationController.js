35ed50588de723ea129f7856e38590db2c7fba6f
kam <kamkam6b@hotmail.com>
2021-04-05 03:16:26 +0800

    collaborative filtering first version

--- controllers/recommendationController.js
+++ controllers/recommendationController.js
@@ -49,6 +49,9 @@ const recommendationRoutes = async (req, res) => {
     
     const top5RecommendedRoutes = contentBased_recommendedHikingRoutes.slice(0, 5)
 
+    // getUtilityMatrix()
+    getSimilarityMatrix()
+
     res.send({
         message: 'recommendation API called!',
         top5recommendedRoutes: top5RecommendedRoutes,
@@ -110,6 +113,119 @@ const getJaccardSimilarityScore = (firstHikingRoute, secondHikingRoute, compareF
     }
 
     return numerator / denominator
+}
+
+const getAllHikingRoutesId = async() => {
+    const allHikingRoutesIdResult = await pool.query(`SELECT id FROM hikingRoutes WHERE deletedAt IS NULL ORDER BY id ASC;`)
+    const allHikingRoutesId = allHikingRoutesIdResult.rows.map((v) => {
+        return v.id;
+    })
+
+    return allHikingRoutesId
+}
+
+const getAllUsersRatingRecords = async() => {
+    const allUsersRatingRecordsResult = await pool.query(`SELECT raterId, hikingRouteId, rating FROM hikingRouteUserRating;`)
+    return allUsersRatingRecordsResult.rows
+}
+
+const getAllRateduserId = async() => {
+    const allRatedUsersIdResult = await pool.query(`SELECT DISTINCT raterId FROM hikingRouteUserRating ORDER BY raterId ASC;`)    
+    const allRatedUsersId = allRatedUsersIdResult.rows.map((v) => {
+        return v.raterid;
+    })
+
+    return allRatedUsersId
+}
+
+const getUtilityMatrix = async () => {
+    const allHikingRoutesId = await getAllHikingRoutesId()
+    const allRatedUsersId = await getAllRateduserId()
+    const allUsersRating = await getAllUsersRatingRecords()
+    const hikingRoutesAmount = allHikingRoutesId.length
+    const ratedUsersAmount = allRatedUsersId.length
+    const utilityMatrix = initializeMatrix(ratedUsersAmount, hikingRoutesAmount, 0)
+
+    for(let i=0; i<allUsersRating.length; i++) {
+        const userRating = allUsersRating[i]
+        const userIndex = allRatedUsersId.indexOf(userRating.raterid)
+        const hikingRouteIndex = allHikingRoutesId.indexOf(userRating.hikingrouteid)
+        utilityMatrix[userIndex][hikingRouteIndex] = userRating.rating
+    }
+
+    // console.log(JSON.stringify(allHikingRoutesId))
+    // printMatrix(utilityMatrix)
+    console.log('utility matrix:')
+    console.table(utilityMatrix)
+    // const routeLabels = allHikingRoutesId.map((v) => {
+    //     return `r${v}`
+    // })
+    // console.table(allRatedUsersId, routeLabels)
+    // console.table([
+    //     { a: 1, b: 2 }, 
+    //     { a: 3, b: 7, c: 'y' }],
+    //     ['a', 'b', 'c']
+    // );
+    return utilityMatrix
+}
+
+const getSimilarityMatrix = async () => {
+    const allHikingRoutesId = await getAllHikingRoutesId()
+    const allRatedUsersId = await getAllRateduserId()
+    const hikingRoutesAmount = allHikingRoutesId.length
+    const similartyMatrix = initializeMatrix(hikingRoutesAmount, hikingRoutesAmount, -1)
+    const utilityMatrix = await getUtilityMatrix()
+
+    // calculate the similarity value here
+    for(let i=0; i<similartyMatrix.length; i++) {
+        for(let j=0; j<similartyMatrix[i].length; j++) {
+            if(i == j) {
+                similartyMatrix[i][j] = 1
+                continue
+            }
+            var numerator = 0;
+            var denominator = 0;
+            var term1 = 0;
+            var term2 = 0;
+            for(let k=0; k<allRatedUsersId.length; k++) {
+                numerator += utilityMatrix[k][i] * utilityMatrix[k][j]
+                term1 += utilityMatrix[k][i] * utilityMatrix[k][i]
+                term2 += utilityMatrix[k][j] * utilityMatrix[k][j]
+            }
+            denominator = Math.sqrt(term1) * Math.sqrt(term2)
+            if(numerator == 0 || denominator == 0) {
+                continue
+            }
+            const similarity = numerator / denominator
+            similartyMatrix[i][j] = similarity
+            // similartyMatrix[i][j] = Math.round(similarity * 1000) / 1000
+        }
+    }
+    // console.log('sim:')
+    console.log('similarity matrix:')
+    // printMatrix(similartyMatrix)
+    console.table(similartyMatrix)
+
+    return similartyMatrix
+}
+
+const initializeMatrix = (row, col, defaultValue) => {
+    const matrix = []
+    for(let i=0; i<row; i++) {
+        const rowArr = []
+        for(let j=0; j<col; j++) {
+            rowArr.push(defaultValue)
+        }
+        matrix.push(rowArr)
+    }
+
+    return matrix
+}
+
+const printMatrix = (matrixArray) => {
+    for(let i=0; i<matrixArray.length; i++) {
+        console.log(JSON.stringify(matrixArray[i]))
+    }
 }
 
 const removeDuplicateItem = (arr) => {
