165d879196c9ff6f9075c9b06e44da7ab46fe429
kam <kamkam6b@hotmail.com>
2021-06-12 16:14:03 +0800

    edited database url to heroku

--- database.js
+++ database.js
@@ -11,10 +11,14 @@ const getDBPool = () => {
         // host: 'kam',
         // port: 5432,
         // database: 'FYP',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME
+        // user: process.env.DB_USER,
+        // password: process.env.DB_PASSWORD,
+        // port: process.env.DB_PORT,
+        // database: process.env.DB_NAME
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     })
     return pool
 };
